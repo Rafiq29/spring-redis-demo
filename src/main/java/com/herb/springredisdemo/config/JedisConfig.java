28e27d2846336f6c4eb4e1d352d2c082b53e074b
Rafiq29 <rafiq.babayev.29@gmail.com>
2022-12-08 15:10:46 +0400

    -- Initial commit

--- src/main/java/com/herb/springredisdemo/config/JedisConfig.java
+++ src/main/java/com/herb/springredisdemo/config/JedisConfig.java
@@ -1,2 +1,21 @@
-package config;public class JedisConfig {
+package com.herb.springredisdemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class JedisConfig {
+    @Bean
+    JedisConnectionFactory jedisConnectionFactory() {
+        return new JedisConnectionFactory();
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        return template;
+    }
 }
